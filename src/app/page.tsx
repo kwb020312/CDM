5d46e2caae8590ba394b1002e46d8accd85ecb50
김우빈 <kwb020312@naver.com>
2023-11-30 21:29:23 +0900

    Feat: Apply Shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
-  return <p>Hello WOrld</p>;
+  return (
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          높은 품질의 <span className="text-blue-600">디지털</span> 마켓
+        </h1>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
