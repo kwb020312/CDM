4e67d9c603c60d6c9a35c5cbbc364b521f8e1f41
김우빈 <kwb020312@naver.com>
2023-12-01 22:22:15 +0900

    Feat: NavBar

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI 관련소스",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "에디터 초이스",
+        href: "#",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "새로운 소스",
+        href: "#",
+        imageSrc: "/nav/ui-kits/blue.jpg",
+      },
+      {
+        name: "베스트 셀러",
+        href: "#",
+        imageSrc: "/nav/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "아이콘 소스",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "에디터 초이스",
+        href: "#",
+        imageSrc: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "새로운 아이콘",
+        href: "#",
+        imageSrc: "/nav/icons/new.jpg",
+      },
+      {
+        name: "베스트 셀러",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
+];
